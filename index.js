5f2a5711442223697310ef4d5f51fbef78668b42
TEJAS9307 <tejaswagh9307@gmail.com>
2025-06-08 23:28:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+=======
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+>>>>>>> 931938a6e32d8dc416bfc003c6817233ed98be18
+}); 
